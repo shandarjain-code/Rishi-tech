aa4d3116a69471df4c83d91bc7ef95ccd7805e47
Rishi <rishijain9343@gmail.com>
2026-07-14 14:14:49 +0530

    Trust proxies instead of forcing scheme to fix Livewire 401

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -21,9 +21,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        if (env('APP_ENV') !== 'local') {
-            \Illuminate\Support\Facades\URL::forceScheme('https');
-        }
         $link = public_path('storage');
         $target = storage_path('app/public');
 
